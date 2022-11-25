b74010a7d40b1e335e5986b63132dbaf2b7412c3
JuanHirschmann <juanhirschmann@gmail.com>
2022-11-24 21:01:23 -0300

    Documentación

---
+++ html/class_shutdown__state.js
@@ -0,0 +1,7 @@
+var class_shutdown__state =
+[
+    [ "enter", "class_shutdown__state.html#ad4e70c7ba007977fd0a1f30d8bc13766", null ],
+    [ "exit", "class_shutdown__state.html#a75110a3a71ad653a0fbd1dff2cd53e90", null ],
+    [ "transition", "class_shutdown__state.html#a5867c2324e8f29a66f0e701592af0734", null ],
+    [ "update", "class_shutdown__state.html#a4cf5de974875a59527b14b4ca2c90855", null ]
+];
