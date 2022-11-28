f5c72653cb94fe8bf2f6cb79c3ed60db9355ad4b
JuanHirschmann <juanhirschmann@gmail.com>
2022-11-27 23:58:59 -0300

    Modificación estilo Doxygen

---
+++ docs/html/dir_68267d1309a1af8e8297ef4c3efbcdba.js
@@ -0,0 +1,9 @@
+var dir_68267d1309a1af8e8297ef4c3efbcdba =
+[
+    [ "Buzzer.cpp", "_buzzer_8cpp.html", null ],
+    [ "Display.cpp", "_display_8cpp.html", null ],
+    [ "Instruction_observer.cpp", "_instruction__observer_8cpp.html", null ],
+    [ "Motor.cpp", "_motor_8cpp.html", null ],
+    [ "Subject.cpp", "_subject_8cpp.html", null ],
+    [ "Temperature_sensor.cpp", "_temperature__sensor_8cpp.html", null ]
+];
